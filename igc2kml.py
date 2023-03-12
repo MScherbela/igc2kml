bd07fa39ee8d7cdfd2b40d01bf9e569ff04537ef
PacketFiend <github-01@voort.ca>
2023-03-12 01:55:08 -0500

    Fix the crazy vario calculations and add more options for speed/distance units

--- igc2kml.py
+++ igc2kml.py
@@ -29,6 +29,14 @@ class FlightData:
     def n_samples(self):
         return len(self.t)
 
+class Units:
+    x: str
+    y: str
+    alt: str
+    xfactor: Optional[float] = None
+    yfactor: Optional[float] = None
+    altfactor: Optional[float] = None
+
 def get_distance(lat1, lon1, lat2, lon2):
     a = math.sin(lat1 * math.pi / 180) * math.sin(lat2 * math.pi / 180)
     b = math.cos(lat1 * math.pi / 180) * math.cos(lat2 * math.pi / 180) * math.cos((lon2 - lon1) * math.pi / 180)
@@ -75,8 +83,7 @@ def parse_igc(fname):
     t, lat, lon, _, _, alt_gps = zip(*data)
     return FlightData(t=t, lat=lat, lon=lon, alt=alt_gps), meta_data
 
-def _write_kml_timeseries(f, data, color_data, color_map_name, cmin, cmax, n_colors, name="", postfix=""):
-    global speed_unit_str
+def _write_kml_timeseries(f, data, color_data, color_map_name, cmin, cmax, n_colors, units, name="", postfix=""):
     f.write("<Folder>\n")
     f.write(f"<name>{name}</name>")
     for i in range(data.n_samples - 1):
@@ -87,7 +94,7 @@ def _write_kml_timeseries(f, data, color_data, color_map_name, cmin, cmax, n_col
         elif ind_color < 0:
             ind_color = 0
         f.write(f'\t<styleUrl>#{color_map_name}{ind_color}</styleUrl>\n')
-        f.write(f'\t<name>{data.t[i].hour}:{data.t[i].minute}:{data.t[i].second}, {data.alt[i]:.0f}m, {data.speed[i]:.0f}{postfix}</name>')
+        f.write(f'\t<name>{data.t[i].hour}:{data.t[i].minute}:{data.t[i].second}, {data.alt[i]*units.altfactor:.0f}{units.alt}, {color_data[i]:.0f}{postfix}</name>')
         f.write('\t<LineString>\n')
         f.write('\t<altitudeMode>absolute</altitudeMode>\n')
         f.write('\t<coordinates>\n')
@@ -134,7 +141,7 @@ def _write_kml_colormap(f, name, values):
     f.write('</Style>\n')
 
 
-def write_kml(fname, data: FlightData, metadata: dict):
+def write_kml(fname, data: FlightData, metadata: dict, units):
     color_maps = dict(rdgn9=["ff2600a5", "ff2e40de", "ff528ef9", "ff81d4fe", "ffbefffe", "ff82e9cb", "ff66ca84", "ff54a02a", "ff376800"])
     with open(fname, 'w') as f:
         f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
@@ -156,8 +163,8 @@ def write_kml(fname, data: FlightData, metadata: dict):
         f.write("<name>Flight track</name>\n")
         f.write("<open>1</open>\n")
         f.write("<Style><ListStyle><listItemType>radioFolder</listItemType></ListStyle></Style>\n")
-        _write_kml_timeseries(f, data, data.vario, 'rdgn9', -4, 4, 9, "Vario [m/s]", "m/s")
-        _write_kml_timeseries(f, data, data.speed, 'rdgn9', 0, 60, 9, f"Speed [{meta_data['speed_unit']}]", meta_data['speed_unit'])
+        _write_kml_timeseries(f, data, data.vario, 'rdgn9', -4, 4, 9, units, f"Vario [{meta_data['units'].y}]", meta_data['units'].y)
+        _write_kml_timeseries(f, data, data.speed, 'rdgn9', 0, 60, 9, units, f"Speed [{meta_data['units'].x}]", meta_data['units'].x)
         f.write("</Folder>")
         _write_kml_path(f, data)
         f.write('</Document>\n')
@@ -175,52 +182,70 @@ def get_nearest_launch_site_name(lat, lon):
     return best_name
 
 
-def process_data(data, meta_data, speed_unit):
+def process_data(data, meta_data, units):
     data.x = [EARTH_RADIUS * math.cos(lat * math.pi / 180) * math.cos(lon * math.pi / 180) for lat, lon in
               zip(data.lat, data.lon)]
     data.y = [EARTH_RADIUS * math.cos(lat * math.pi / 180) * math.sin(lon * math.pi / 180) for lat, lon in
               zip(data.lat, data.lon)]
-    data.vario = [data.alt[i + 1] - data.alt[i] for i in range(data.n_samples - 1)] + [0]
     timedelta = [(data.t[i+1] - data.t[i]).seconds for i in range(data.n_samples -1)] + [1.0]
     distance_delta = [math.sqrt((data.x[i + 1] - data.x[i]) ** 2 + (data.y[i + 1] - data.y[i]) ** 2) for i in
                       range(data.n_samples - 1)] + [0.0]
-    speed_conversion_factor = get_speed_conversion_factor(speed_unit)
-    data.speed = [speed_conversion_factor * dx / dt for dx, dt in zip(distance_delta, timedelta)]
+    vario_delta = [data.alt[i + 1] - data.alt[i] for i in range(data.n_samples - 1)] + [0.0]
+    units.xfactor = get_conversion_factor(units.x)
+    units.yfactor = get_conversion_factor(units.y)
+    units.altfactor = get_conversion_factor(units.alt)
+    # Some recording devices take multiple fixes per minute, but only record timestamps accurate to the minute.
+    # Adding one millisecond avoids division by zero exceptions, but fubars the speed calculations.
+    # A better method would be to calculate the frequency of fixes in these cases. This is an ugly hack.
+    data.speed = [units.xfactor * dx / (dt+0.001) for dx, dt in zip(distance_delta, timedelta)]
+    data.vario = [units.yfactor * dy / (dt+0.001) for dy, dt in zip(vario_delta, timedelta)]
     site_name = get_nearest_launch_site_name(data.lat[0], data.lon[0])
     meta_data['launch_site'] = site_name
-    meta_data['speed_unit'] = speed_unit
+    meta_data['units'] = units
     return data, meta_data
 
-def get_speed_conversion_factor(unit):
+def get_conversion_factor(unit):
+    """Gets a conversion factor from metres per second (or simple metres) to whatever your heart desires"""
+    # I bet there's a conversion library that would be better for this
     if unit in ["kts", "knots"]:
         return 1.9438444924406
     elif unit in ["mph", "miles/h"]:
         return 2.2369362920544
     elif unit in ["kmh", "km/h"]:
         return 3.6
-    elif unit in ["m/s", "mps"]:
+    elif unit in ["fpm", "f/m"]:
+        return 196.85039370078738
+    elif unit in ["feet"]:
+        return 3.280839895013123
+    elif unit in ["m/s", "mps", "m", "metres", "meters"]:
         return 1.0
     else:
         raise ValueError(f"Unknown unit for speed: {unit}")
 
 if __name__ == '__main__':
+    units = Units()
     parser = argparse.ArgumentParser(description="IGC to KML converter for flight logs, so they can be viewed with Google Earth.")
     parser.add_argument("input", nargs="+", help="Input file name(s)")
     parser.add_argument("--output", help="Output file name", default=None)
     parser.add_argument("--force", "-f", help="Overwrite output file if it exists", action="store_true")
     parser.add_argument("--pilot", "-p", help="Pilot's name (will appear on flight path name)", type=str)
-    parser.add_argument("--units", "-u", help="Ground speed units, kts or kmh (default kmh)", type=str, choices=["m/s", "kmh", "mph", "kts"], default="kmh")
+    parser.add_argument("--xunits", "-x", help="Ground speed units (default kmh)", type=str, choices=["m/s", "kmh", "mph", "kts", "knots", "miles/h"], default="kmh")
+    parser.add_argument("--yunits", "-y", help="Vertical speed units (default mps)", type=str, choices=["m/s", "kmh", "mph", "kts", "fpm", "f/m"], default="m/s")
+    parser.add_argument("--altunits", "-a", help="Altitude units (default m)", type=str, choices=["m", "feet", "metres", "metres"], default="m")
     args = parser.parse_args()
+    units.x = args.xunits
+    units.y = args.yunits
+    units.alt = args.altunits
 
     for input_fname in args.input:
         data, meta_data = parse_igc(input_fname)
-        data, meta_data = process_data(data, meta_data, speed_unit=args.units)
+        data, meta_data = process_data(data, meta_data, units)
         meta_data["pilot"] = args.pilot
         output_name = args.output or f"{data.t[0]:%Y_%m_%d_%H%M}_{meta_data['launch_site']}.kml"
         if os.path.isfile(output_name) and not args.force:
             print(f"Can not save kml file, because it already exists: {output_name}")
         else:
-            write_kml(output_name, data, meta_data)
+            write_kml(output_name, data, meta_data, units)
 
 
 
